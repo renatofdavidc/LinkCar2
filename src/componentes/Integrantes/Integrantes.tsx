c17a3041eede1c15b771fa06bf08a99c8c9a44f1
Renato David <renatofdavidc@gmail.com>
2024-09-13 20:49:18 -0300

    ultimo commit

--- src/componentes/Integrantes/Integrantes.tsx
+++ src/componentes/Integrantes/Integrantes.tsx
@@ -2,11 +2,24 @@
 const Integrantes = () =>{
     return(
         <>
-        <h1>Integrantes</h1>
         <main>
             <body>
                 <div className="content">
-                    
+                    <h1 className="titulo_pagina">Integrantes</h1>
+                    <div className="container_integrantes">
+                        <div className="quadro_imagem">
+                            <div id="foto_renato"><img src="/public/imagem/renatosantista.png" alt="" /></div>
+                            <p className="nomes">Renato de Freitas David Campiteli <br />RM: 555627 <br /> <a href="https://github.com/renatofdavidc">Github</a></p>
+                        </div>
+                        <div className="quadro_imagem">
+                            <div id="foto_pedro"><img src="/public/imagem/pedrovaisantos.png" alt="" /></div>
+                            <p className="nomes">Pedro Lucas de Oliveira Bezerra <br />RM: 558439 <br /> <a href="https://github.com/PedrinDev1447">Github</a></p>
+                        </div>
+                        <div className="quadro_imagem">
+                            <div id="foto_jaba"><img src="/public/imagem/jabapexao.png" alt="" /></div>
+                            <p className="nomes">Gabriel Santos Jablonski <br />RM: 555425 <br /><a href="https://github.com/Jablonski17">Github</a></p>
+                        </div>
+                    </div>        
                 </div>
             </body>
         </main>
